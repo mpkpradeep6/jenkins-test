85fd079a79b9da1089f7fc5fcce1e6e966e0569a
cpmsTest <markpatrickwhite@hotmail.com>
2016-04-25 15:31:23 -0400

    Initial project version
    This is the first upload.

---
+++ BuildTestApp1/BuildTestApp1/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildTestApp1
+{
+    public class Calculator
+    {
+        private int _total;
+        public Calculator() { _total = 0; }
+
+        public void Add(int number)
+        {
+            _total += number;
+        }
+
+        public int Total { get { return _total; } }
+    }
+}
---
+++ BuildTestApp1/BuildTestApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildTestApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var calc = new Calculator();
+            calc.Add(2);
+            calc.Add(3);
+            Console.WriteLine("Calculator Total: {0}", calc.Total);
+        }
+    }
+}
